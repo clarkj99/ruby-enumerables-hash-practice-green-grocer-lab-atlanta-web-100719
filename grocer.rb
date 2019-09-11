f60ec121c905633e60236a80c5a0eef988babdec
Clark Johnson <clarkandkathy@gmail.com>
2019-09-11 04:36:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,51 @@
 def consolidate_cart(cart)
-  # code here
+  cart.reduce({}) do |memo, item| 
+    item.each do |key,value| 
+      value[:count]=cart.count(item)
+      memo[key]=value 
+    end  
+    memo 
+  end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = cart
+  coupons.each do |coupon|
+    if (new_cart[coupon[:item]] != nil) && (new_cart[coupon[:item]][:count] >= coupon[:num])
+      new_cart[coupon[:item]][:count] -= coupon[:num] 
+      if new_cart[coupon[:item] + " W/COUPON"] == nil
+        new_cart[coupon[:item] + " W/COUPON"] = {
+          :price => coupon[:cost] / coupon[:num], 
+          :clearance => new_cart[coupon[:item]][:clearance],
+          :count => coupon[:num]
+        }
+      else
+        new_cart[coupon[:item] + " W/COUPON"][:count] += coupon[:num]
+      end
+    end 
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.map do |key, value|
+    if value[:clearance]
+      value[:price] = (value[:price] * 0.80).round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
   # code here
+end
+
+def consolidate_cart2(cart)
+  new_hash = Hash.new
+  cart.uniq.each do |item| 
+    item.each do |key, value|
+      value [:count] = cart.count(item)
+      new_hash [key] = value
+    end 
+  end 
+  new_hash
 end
