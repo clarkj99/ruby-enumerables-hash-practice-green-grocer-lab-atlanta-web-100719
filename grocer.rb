28908cbaeb45af67bd81e0f99271397fae1b663b
Clark Johnson <clarkandkathy@gmail.com>
2019-09-11 04:40:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  cart.map do |item, (key, value)|
+  cart.each do |item, (key, value)|
     if value[:clearance]
       value[:price] = (value[:price] * 0.80).round(2)
     end
